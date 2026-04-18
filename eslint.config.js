43c46a1f4534eb98d9c71a934ecc084ebf626d14
Ajit Bohra <ajit@lubus.in>
2026-04-18 19:36:46 +0530

    chore(quality): add lint typecheck test and build automation

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginVue from 'eslint-plugin-vue';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['node_modules/**', '.vite/**', 'out/**', 'docs/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.{ts,tsx,vue}'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  }
+);
